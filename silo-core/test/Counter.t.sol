d7311f2316c380d356322734e1e502dcf0d0b89b
techsaavy8784 <blueskydev1205@gmail.com>
2023-04-24 16:12:28 +0200

    example of setup new workspace and foundry

--- silo-core/test/Counter.t.sol
+++ silo-core/test/Counter.t.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.7.6;
+pragma abicoder v2;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../contracts/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
