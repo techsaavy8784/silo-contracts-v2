ffaa25dbc0e2cb8689fbf379b8ca2e17b57fa60d
techsaavy8784 <blueskydev1205@gmail.com>
2023-08-30 22:51:44 +0200

    adapt UniswapV3 oracle to clone + config flow

--- silo-core/contracts/interface/ISiloOracle.sol
+++ silo-core/contracts/interface/ISiloOracle.sol
@@ -2,8 +2,15 @@
 pragma solidity >=0.5.0;
 
 interface ISiloOracle {
+    /// @return quoteAmount Returns quote price for _baseAmount of _baseToken
+    /// @param _baseAmount Amount of priced token
+    /// @param _baseToken Address of priced token
     function quote(uint256 _baseAmount, address _baseToken) external returns (uint256 quoteAmount);
+
+    /// @return quoteAmount Returns quoteAmount quote price for _baseAmount of _baseToken
+    /// @param _baseAmount Amount of priced token
+    /// @param _baseToken Address of priced token
     function quoteView(uint256 _baseAmount, address _baseToken) external view returns (uint256 quoteAmount);
+
     function quoteToken() external view returns (address);
-    function baseToken() external view returns (address);
 }
