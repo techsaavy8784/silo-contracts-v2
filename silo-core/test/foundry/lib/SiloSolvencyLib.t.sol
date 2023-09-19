25eefd10a3bac57df3624d8589448842ad54522c
techsaavy8784 <blueskydev1205@gmail.com>
2023-09-19 17:03:38 +0200

    qa: SiloSolvencyLib.getPositionValues()

---
+++ silo-core/test/foundry/lib/SiloSolvencyLib.t.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../../../contracts/lib/SiloSolvencyLib.sol";
+
+
+// forge test -vv --mc SiloSolvencyLibTest
+contract SiloSolvencyLibTest is Test {
+    /*
+    forge test -vv --mt test_SiloSolvencyLib_getPositionValues_noOracle
+    */
+    function test_SiloSolvencyLib_getPositionValues_noOracle() public {
+        ISiloOracle noOracle;
+        uint256 protectedAssets = 10;
+        uint256 collateralAssets = 20;
+        uint256 debtAssets = 3;
+
+        SiloSolvencyLib.LtvData memory ltvData = SiloSolvencyLib.LtvData(
+            noOracle, noOracle, protectedAssets, collateralAssets, debtAssets
+        );
+
+        address any = address(1);
+
+        (uint256 collateralValue, uint256 debtValue) = SiloSolvencyLib.getPositionValues(ltvData, any, any);
+
+        assertEq(collateralValue, collateralAssets + protectedAssets, "collateralValue");
+        assertEq(debtValue, debtAssets, "debtValue");
+    }
+
+    /*
+    forge test -vv --mt test_SiloSolvencyLib_getPositionValues_pass
+    */
+    function test_SiloSolvencyLib_getPositionValues_pass() public {
+        address collateralOracle = address(0x555555);
+        address debtOracle = address(0x77777);
+        uint256 protectedAssets = 10;
+        uint256 collateralAssets = 20;
+        uint256 debtAssets = 3;
+
+        SiloSolvencyLib.LtvData memory ltvData = SiloSolvencyLib.LtvData(
+            ISiloOracle(debtOracle),
+            ISiloOracle(collateralOracle),
+            protectedAssets,
+            collateralAssets,
+            debtAssets
+        );
+
+        address collateralAsset = address(0xc01a);
+        address debtAsset = address(0xdeb);
+
+        vm.mockCall(
+            collateralOracle,
+            abi.encodeWithSelector(ISiloOracle.quote.selector, ltvData.collateralAssets + ltvData.protectedAssets, collateralAsset),
+            abi.encode(uint256(9876))
+        );
+
+        vm.mockCall(
+            debtOracle,
+            abi.encodeWithSelector(ISiloOracle.quote.selector, ltvData.debtAssets, debtAsset),
+            abi.encode(uint256(1234))
+        );
+
+        (
+            uint256 collateralValue, uint256 debtValue
+        ) = SiloSolvencyLib.getPositionValues(ltvData, collateralAsset, debtAsset);
+
+        assertEq(collateralValue, 9876, "collateralValue");
+        assertEq(debtValue, 1234, "debtValue");
+    }
+}
