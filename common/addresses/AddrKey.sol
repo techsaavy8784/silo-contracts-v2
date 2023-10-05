7bb41897399de68d123cd169b78c19833d9c764a
IhorSF <102213749+IhorSF@users.noreply.github.com>
2023-10-05 17:48:06 +0400

    Silo-oracles: deployments (#167)

---
+++ common/addresses/AddrKey.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.2 <0.9.0;
+
+library AddrKey {
+    string constant public WETH = "WETH";
+    string constant public BALANCER_VAULT = "BALANCER_VAULT";
+    string constant public UNISWAP_ROUTER = "UNISWAP_ROUTER";
+    string constant public UNISWAP_FACTORY = "UNISWAP_FACTORY";
+    string constant public USDC = "USDC";
+    string constant public USDC_ETH_UNI_POOL = "USDC_ETH_UNI_POOL";
+}
