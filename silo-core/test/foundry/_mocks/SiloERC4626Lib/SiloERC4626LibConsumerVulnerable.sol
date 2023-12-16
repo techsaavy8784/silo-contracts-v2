b1310c4bc8d9115e4d5914648a1ddb657080e7b0
IhorSF <102213749+IhorSF@users.noreply.github.com>
2023-12-16 08:53:58 +0400

    TOB-SILO2-20: Deposit reentrancy in SiloERC4626Lib (#325)

---
+++ silo-core/test/foundry/_mocks/SiloERC4626Lib/SiloERC4626LibConsumerVulnerable.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.21;
+
+import {ISilo} from "silo-core/contracts/interfaces/ISilo.sol";
+import {IShareToken} from "silo-core/contracts/interfaces/IShareToken.sol";
+import {SiloERC4626LibWithReentrancyIssue} from "./SiloERC4626LibWithReentrancyIssue.sol";
+
+contract SiloERC4626LibConsumerVulnerable {
+    uint256 public constant INITIAL_TOTAL = 100;
+
+    mapping(ISilo.AssetType => ISilo.Assets) internal _total;
+
+    constructor() {
+        _total[ISilo.AssetType.Collateral].assets = INITIAL_TOTAL;
+    }
+
+    function deposit(
+        address _token,
+        address _depositor,
+        uint256 _assets,
+        uint256 _shares,
+        address _receiver,
+        IShareToken _collateralShareToken,
+        IShareToken _debtShareToken
+    ) public {
+        SiloERC4626LibWithReentrancyIssue.deposit(
+            _token,
+            _depositor,
+            _assets,
+            _shares,
+            _receiver,
+            _collateralShareToken,
+            _debtShareToken,
+            _total[ISilo.AssetType.Collateral]
+        );
+    }
+
+    function getTotalCollateral() public view returns (uint256) {
+        return _total[ISilo.AssetType.Collateral].assets;
+    }
+}
