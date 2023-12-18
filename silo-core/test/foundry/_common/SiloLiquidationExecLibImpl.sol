a8fabeb3168990c4f9bcd994771d6c6b881fcfb8
Thomas A. Anderson <92584446+0xNeoRacer@users.noreply.github.com>
2023-12-18 13:25:58 +0100

    TOB-SILO2-18: Minimum acceptable LTV is not enforced for liquidation

--- silo-core/test/foundry/_common/SiloLiquidationExecLibImpl.sol
+++ silo-core/test/foundry/_common/SiloLiquidationExecLibImpl.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.0;
 
 import {SiloSolvencyLib} from "silo-core/contracts/lib/SiloSolvencyLib.sol";
+import {SiloLiquidationLib} from "silo-core/contracts/lib/SiloLiquidationLib.sol";
 import {SiloLiquidationExecLib} from "silo-core/contracts/lib/SiloLiquidationExecLib.sol";
 
 contract SiloLiquidationExecLibImpl {
     function liquidationPreview(
         SiloSolvencyLib.LtvData memory _ltvData,
-        SiloLiquidationExecLib.LiquidationPreviewParams memory _params
+        SiloLiquidationLib.LiquidationPreviewParams memory _params
     )
         external
         view
