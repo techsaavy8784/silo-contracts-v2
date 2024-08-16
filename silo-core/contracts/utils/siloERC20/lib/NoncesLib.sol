262dd521b0783733329594d8f01c32350d578b44
IhorSF <102213749+IhorSF@users.noreply.github.com>
2024-08-16 14:03:31 +0400

    silo-core: ERC20Permit (#636)

---
+++ silo-core/contracts/utils/siloERC20/lib/NoncesLib.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v5.0.0) (utils/Nonces.sol)
+pragma solidity ^0.8.20;
+
+/**
+ * @dev Provides tracking nonces for addresses. Nonces will only increment.
+ */
+library NoncesLib {
+    /**
+     * @dev The nonce used for an `account` is not the expected current nonce.
+     */
+    error InvalidAccountNonce(address account, uint256 currentNonce);
+
+    /// @custom:storage-location erc7201:openzeppelin.storage.Nonces
+    struct NoncesStorage {
+        mapping(address account => uint256) _nonces;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.Nonces")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant NoncesStorageLocation = 0x5ab42ced628888259c08ac98db1eb0cf702fc1501344311d8b100cd1bfe4bb00;
+
+    function _getNoncesStorage() private pure returns (NoncesStorage storage $) {
+        assembly {
+            $.slot := NoncesStorageLocation
+        }
+    }
+
+    /**
+     * @dev Returns the next unused nonce for an address.
+     */
+    function nonces(address owner) public view returns (uint256) {
+        NoncesStorage storage $ = _getNoncesStorage();
+        return $._nonces[owner];
+    }
+
+    /**
+     * @dev Consumes a nonce.
+     *
+     * Returns the current value and increments nonce.
+     */
+    function _useNonce(address owner) internal returns (uint256) {
+        NoncesStorage storage $ = _getNoncesStorage();
+        // For each account, the nonce has an initial value of 0, can only be incremented by one, and cannot be
+        // decremented or reset. This guarantees that the nonce never overflows.
+        unchecked {
+            // It is important to do x++ and not ++x here.
+            return $._nonces[owner]++;
+        }
+    }
+
+    /**
+     * @dev Same as {_useNonce} but checking that `nonce` is the next valid for `owner`.
+     */
+    function _useCheckedNonce(address owner, uint256 nonce) internal {
+        uint256 current = _useNonce(owner);
+        if (nonce != current) {
+            revert InvalidAccountNonce(owner, current);
+        }
+    }
+}
