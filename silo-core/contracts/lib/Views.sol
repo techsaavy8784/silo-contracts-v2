6ae8324969a0b3877830e11a15d98ba88c813298
Thomas A. Anderson <92584446+0xNeoRacer@users.noreply.github.com>
2024-10-14 07:35:42 +0200

    silo-core: remove redundant check for `callBeforeQuote` (#781)

--- silo-core/contracts/lib/Views.sol
+++ silo-core/contracts/lib/Views.sol
@@ -176,7 +176,7 @@ library Views {
         configData0.deployerFee = _initData.deployerFee;
         configData0.liquidationFee = _initData.liquidationFee0;
         configData0.flashloanFee = _initData.flashloanFee0;
-        configData0.callBeforeQuote = _initData.callBeforeQuote0 && configData0.maxLtvOracle != address(0);
+        configData0.callBeforeQuote = _initData.callBeforeQuote0;
 
         configData1.hookReceiver = _initData.hookReceiver;
         configData1.token = _initData.token1;
@@ -191,7 +191,7 @@ library Views {
         configData1.deployerFee = _initData.deployerFee;
         configData1.liquidationFee = _initData.liquidationFee1;
         configData1.flashloanFee = _initData.flashloanFee1;
-        configData1.callBeforeQuote = _initData.callBeforeQuote1 && configData1.maxLtvOracle != address(0);
+        configData1.callBeforeQuote = _initData.callBeforeQuote1;
     }
 
     function validateSiloInitData(
