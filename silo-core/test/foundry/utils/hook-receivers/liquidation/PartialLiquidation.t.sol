87537524d7e9f0477d63ad470380ea5e9bf928fa
Thomas A. Anderson <92584446+0xNeoRacer@users.noreply.github.com>
2024-10-15 16:35:36 +0200

    silo-core: qa hook actions for partial liquidation (#725)

---
+++ silo-core/test/foundry/utils/hook-receivers/liquidation/PartialLiquidation.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+
+import {IHookReceiver} from "silo-core/contracts/interfaces/IHookReceiver.sol";
+
+import {SiloLittleHelper} from "../../../_common/SiloLittleHelper.sol";
+
+/*
+    forge test -vv --ffi --mc PartialLiquidationTest
+*/
+contract PartialLiquidationTest is SiloLittleHelper, Test {
+    function setUp() public {
+        _setUpLocalFixture();
+    }
+
+    function test_partialLiquidation_beforeAction() public {
+        // can be called
+        IHookReceiver(address(partialLiquidation)).beforeAction(address(0), 0, "");
+    }
+
+    function test_partialLiquidation_afterAction() public {
+        // can be called
+        IHookReceiver(address(partialLiquidation)).afterAction(address(0), 0, "");
+    }
+}
